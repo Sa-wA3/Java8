0add250cee97cff3fdd32c7e27cb54ab84a85360
Takayuki Hirasawa <t-hirasawa@atware.co.jp>
2021-06-17 11:11:02 +0900

    終端操作色々試した

--- StreamSample.java
+++ StreamSample.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamSample {
@@ -13,35 +15,32 @@ public class StreamSample {
         number_list.add(3);
         number_list.add(4);
 
-        Stream<Integer> stream = number_list.stream();
-        stream.mapToDouble(x -> x + 1).forEach(System.out::println);
+        // Stream<Integer> stream = number_list.stream();
+        // stream.mapToDouble(x -> x + 1).forEach(System.out::println);
 
-        Stream<Integer> stream2 = number_list.stream();
-        stream2.map(x -> x * 4).forEach(System.out::println);
+        // Stream<Integer> stream2 = number_list.stream();
+        // stream2.map(x -> x * 4).forEach(System.out::println);
 
-        List<String> name_list = new ArrayList<>();
-        name_list.add("hirasawa");
-        name_list.add("yoshii");
-        name_list.add("nakayama");
-        name_list.add("nishikawa");
+        // List<String> name_list = new ArrayList<>();
+        // name_list.add("hirasawa");
+        // name_list.add("yoshii");
+        // name_list.add("nakayama");
+        // name_list.add("nishikawa");
 
-        Stream<String> stream3 = name_list.stream();
-        stream3.map(name -> name.length()).forEach(System.out::println);
+        // Stream<String> stream3 = name_list.stream();
+        // stream3.map(name -> name.length()).forEach(System.out::println);
 
-        Stream<String> stream4 = name_list.stream();
-        stream4.map(name -> name.charAt(2)).forEach(System.out::println);
+        // Stream<String> stream4 = name_list.stream();
+        // stream4.map(name -> name.charAt(2)).forEach(System.out::println);
 
         List<Integer> number_list2 = Arrays.asList(1,3,5,4,2,6,8,7,9,10);
         Stream<Integer> stream5 = number_list2.stream();
         stream5.filter(x -> x >= 5).forEach(System.out::println);
 
         Stream<Integer> stream6 = number_list2.stream();
-        stream6.limit(3).forEach(System.out::println);
+        stream6.limit(3).collect(Collectors.toList());
 
         Stream<Integer> stream7 = number_list2.stream();
-        stream7.distinct().forEach(System.out::println);
-
-        Stream<Integer> stream8 = number_list2.stream();
-        stream8.sorted((x,y) -> x - y).forEach(System.out::print);
+        stream7.sorted((x,y) -> x - y).collect(Collectors.toList());
     }
 }
